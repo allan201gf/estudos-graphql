09e61d0910fcf178b00fbec3488bdf0cb609b56d
Allan Ferreira <allan.ferreira@userargo.com>
2022-05-02 10:26:12 -0300

    Hello world e soma com GraphQL

---
+++ curso-graphql-springboot-ricardo-lacheta/compras/src/main/java/br/com/allangf/compras/QueryGraphQL.java
@@ -0,0 +1,17 @@
+package br.com.allangf.compras;
+
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import org.springframework.stereotype.Component;
+
+@Component
+public class QueryGraphQL implements GraphQLQueryResolver {
+
+    public String hello() {
+        return "Hello GraphQL";
+    }
+
+    public int soma(int a, int b) {
+        return a+b;
+    }
+
+}
