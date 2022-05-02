a1049850e2dda12ddfdf68f3c709d287ebcba54b
Allan Ferreira <allan.ferreira@userargo.com>
2022-05-02 11:19:32 -0300

    Type Cliente

---
+++ curso-graphql-springboot-ricardo-lacheta/compras/src/main/java/br/com/allangf/compras/Cliente.java
@@ -0,0 +1,16 @@
+package br.com.allangf.compras;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cliente {
+    String nome;
+    String email;
+
+}
