6cfd11c54d511c54016f77b6f06e690fef9dcba4
Allan Ferreira <allan.ferreira@userargo.com>
2022-05-03 13:31:07 -0300

    Add e delete cliente e organização do código

--- curso-graphql-springboot-ricardo-lacheta/compras/src/main/java/br/com/allangf/compras/graphql/ClienteGraphQL.java
+++ curso-graphql-springboot-ricardo-lacheta/compras/src/main/java/br/com/allangf/compras/graphql/ClienteGraphQL.java
@@ -1,7 +1,7 @@
 package br.com.allangf.compras.graphql;
 
 import br.com.allangf.compras.Cliente;
-import br.com.allangf.compras.ClienteRepository;
+import com.coxautodev.graphql.tools.GraphQLMutationResolver;
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -9,17 +9,29 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 
 @Component
-public class ClienteGraphQL implements GraphQLQueryResolver {
+public class ClienteGraphQL implements GraphQLQueryResolver, GraphQLMutationResolver {
 
     @Autowired
-    private ClienteRepository clienteRepository;
+    private ClienteService clienteService;
 
     public Cliente cliente(Long id) {
-        return clienteRepository.findById(id).orElse(null);
+        return clienteService.findById(id);
     }
 
     public List<Cliente> clientes() {
-        return clienteRepository.findAll();
+        return clienteService.findAll();
+    }
+
+    public Cliente saveCliente(Long id, String nome, String email) {
+        Cliente cliente = new Cliente();
+        cliente.setId(id);
+        cliente.setEmail(email);
+        cliente.setNome(nome);
+        return clienteService.save(cliente);
+    }
+
+    public Boolean deleteCliente(Long id) {
+        return clienteService.deleteById(id);
     }
 
 }
---
+++ curso-graphql-springboot-ricardo-lacheta/compras/src/main/java/br/com/allangf/compras/graphql/ClienteService.java
@@ -0,0 +1,40 @@
+package br.com.allangf.compras.graphql;
+
+import br.com.allangf.compras.Cliente;
+import br.com.allangf.compras.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class ClienteService {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    public Cliente findById(Long id) {
+        return clienteRepository.findById(id).orElse(null);
+    }
+
+    public List<Cliente> findAll() {
+        return clienteRepository.findAll();
+    }
+
+    @Transactional
+    public Cliente save(Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
+
+    @Transactional
+    public Boolean deleteById(Long id) {
+        if(clienteRepository.findById(id).isPresent()) {
+            clienteRepository.deleteById(id);
+            return true;
+        }
+
+        return false;
+    }
+
+}
