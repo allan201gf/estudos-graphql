01a5d260483fb0fe8b2691a563b18e84df7c9dba
Allan Ferreira <allan.ferreira@userargo.com>
2022-05-03 13:10:24 -0300

    Organizando os arquivos do GraphQL

--- curso-graphql-springboot-ricardo-lacheta/compras/src/main/java/br/com/allangf/compras/graphql/ClienteGraphQL.java
+++ curso-graphql-springboot-ricardo-lacheta/compras/src/main/java/br/com/allangf/compras/graphql/ClienteGraphQL.java
@@ -1,25 +1,18 @@
-package br.com.allangf.compras;
+package br.com.allangf.compras.graphql;
 
+import br.com.allangf.compras.Cliente;
+import br.com.allangf.compras.ClienteRepository;
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Component
-public class QueryGraphQL implements GraphQLQueryResolver {
+public class ClienteGraphQL implements GraphQLQueryResolver {
 
     @Autowired
     private ClienteRepository clienteRepository;
-
-    public String hello() {
-        return "Hello GraphQL";
-    }
-
-    public int soma(int a, int b) {
-        return a+b;
-    }
 
     public Cliente cliente(Long id) {
         return clienteRepository.findById(id).orElse(null);
